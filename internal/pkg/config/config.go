a16a7a0a83e98c14c9e2b6678939f03edc2baa35
kemal <atdayewkemal@gmail.com>
2023-09-06 14:23:54 +0500

    config file logic is set up

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -1,1 +1,28 @@
+// config.go
 package config
+
+import (
+	"os"
+)
+
+// Config holds the application configuration.
+type Config struct {
+    DBHost     string
+    DBPort     string
+    DBUser     string
+    DBPassword string
+    DBName     string
+    GRPCPort   string
+}
+
+// LoadConfig loads the application configuration from environment variables.
+func LoadConfig() Config {
+    return Config{
+        DBHost:     os.Getenv("DB_HOST"),
+        DBPort:     os.Getenv("DB_PORT"),
+        DBUser:     os.Getenv("DB_USER"),
+        DBPassword: os.Getenv("DB_PASSWORD"),
+        DBName:     os.Getenv("DB_NAME"),
+        GRPCPort:   os.Getenv("GRPC_PORT"),
+    }
+}
