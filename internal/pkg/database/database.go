b79e4ec0d0d8f86b91141caa04465dd0ccefd838
kemal <atdayewkemal@gmail.com>
2023-09-11 17:30:22 +0500

    database connection improvements

--- internal/pkg/database/database.go
+++ internal/pkg/database/database.go
@@ -1,1 +1,30 @@
 package database
+
+import (
+	"database/sql"
+
+	// Import the PostgreSQL driver
+	_ "github.com/lib/pq"
+)
+
+// Database holds the database connection.
+type Database struct {
+    db *sql.DB
+}
+
+// NewDatabase creates a new Database instance and establishes a database connection.
+func NewDatabase(dbURL string) (*Database, error) {
+    db, err := sql.Open("postgres", dbURL)
+    if err != nil {
+        return nil, err
+    }
+
+    return &Database{db}, nil
+}
+
+// Close closes the database connection.
+func (d *Database) Close() {
+    if d.db != nil {
+        d.db.Close()
+    }
+}
