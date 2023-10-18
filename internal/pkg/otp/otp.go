dbe8b24ba54a6896dfabbec6646fe481a0d39d3b
kemal <atdayewkemal@gmail.com>
2023-10-18 15:17:20 +0500

    OTP code generation function is added

--- internal/pkg/otp/otp.go
+++ internal/pkg/otp/otp.go
@@ -1,6 +1,9 @@
 package otp
 
 import (
+	"math/rand"
+	"time"
+
 	pb "github.com/hojamuhammet/go-grpc-otp-rabbitmq/gen"
 	"github.com/hojamuhammet/go-grpc-otp-rabbitmq/internal/pkg/config"
 	"github.com/hojamuhammet/go-grpc-otp-rabbitmq/internal/pkg/database"
@@ -17,4 +20,14 @@ func NewOTPService(cfg *config.Config, db *database.Database) *OTPService {
 		cfg: cfg,
 		db: db,
 	}
+}
+
+func GenerateOTP() int {
+	// Use the current Unix timestamp (in seconds) as a seed for randomness
+	rand.Seed(time.Now().Unix())
+
+	// Generate a random 6-digit integer OTP
+	otpCode := 100000 + rand.Intn(900000)
+
+	return otpCode
 }
