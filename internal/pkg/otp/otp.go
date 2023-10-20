22f8aacf5cb1239767025378b3208891d2bad2da
kemal <atdayewkemal@gmail.com>
2023-10-20 13:38:19 +0500

    RegisterUser, checkUserExistanceInDatabase, updateUserOTP and storeUserInDatabase functions are added

--- internal/pkg/otp/otp.go
+++ internal/pkg/otp/otp.go
@@ -1,12 +1,17 @@
 package otp
 
 import (
+	"context"
+	"database/sql"
+	"log"
 	"math/rand"
 	"time"
 
 	pb "github.com/hojamuhammet/go-grpc-otp-rabbitmq/gen"
 	"github.com/hojamuhammet/go-grpc-otp-rabbitmq/internal/pkg/config"
 	"github.com/hojamuhammet/go-grpc-otp-rabbitmq/internal/pkg/database"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type OTPService struct {
@@ -22,12 +27,114 @@ func NewOTPService(cfg *config.Config, db *database.Database) *OTPService {
 	}
 }
 
+// RegisterUser handles user registration and OTP generation.
+func (s *OTPService) RegisterUser(ctx context.Context, req *pb.RegisterUserRequest) (*pb.RegisterUserResponse, error) {
+    phoneNumber := req.PhoneNumber
+
+    // Check if the user with the given phone number exists
+    exists, err := s.checkUserExistenceInDatabase(phoneNumber)
+    if err != nil {
+        log.Printf("Error checking user existence in the database: %v", err)
+        return nil, status.Error(codes.Internal, "Failed to register user")
+    }
+
+    var otpCode int32
+    if exists {
+        // User exists, update their OTP in the database
+        otp := GenerateOTP()
+        otpCode = int32(otp)
+        if err := s.updateUserOTP(phoneNumber, int(otp)); err != nil {
+            log.Printf("Error updating OTP in the database: %v", err)
+            return nil, status.Error(codes.Internal, "Failed to register user")
+        }
+    } else {
+        // User doesn't exist, create a new user and store the OTP
+        otp := GenerateOTP()
+        otpCode = int32(otp)
+        if err := s.storeUserInDatabase(phoneNumber, int(otp)); err != nil {
+            log.Printf("Error creating a new user and storing OTP: %v", err)
+            return nil, status.Error(codes.Internal, "Failed to register user")
+        }
+    }
+
+    // Prepare the response message
+    response := &pb.RegisterUserResponse{
+        User: &pb.User{
+            PhoneNumber: phoneNumber,
+            Otp:         otpCode,
+        },
+    }
+
+    log.Printf("Registered user with phone number: %s", phoneNumber)
+
+    // Return the response
+    return response, nil
+}
+
 func GenerateOTP() int {
-	// Use the current Unix timestamp (in seconds) as a seed for randomness
-	rand.Seed(time.Now().Unix())
+    // Use the current Unix timestamp (in seconds) as a seed for randomness
+    rand.Seed(time.Now().Unix())
 
-	// Generate a random 6-digit integer OTP
-	otpCode := 100000 + rand.Intn(900000)
+    // Generate a random 6-digit integer OTP
+    otpCode := 100000 + rand.Intn(900000)
 
-	return otpCode
+    return otpCode
+}
+
+
+func (s *OTPService) checkUserExistenceInDatabase(phoneNumber string) (bool, error) {
+    // Query the database to check if a user with the given phone number exists
+    var exists bool
+    err := s.db.DB.QueryRow("SELECT EXISTS(SELECT 1 FROM users WHERE phone_number = $1)", phoneNumber).Scan(&exists)
+    if err != nil {
+        log.Printf("Error checking user existence in the database: %v", err)
+
+        // Check if the error is due to a database connection issue
+        if err == sql.ErrNoRows {
+            return false, nil
+        }
+
+        return false, err
+    }
+
+    return exists, nil
+}
+
+// updateUserOTP updates the OTP for a user in the database.
+func (s *OTPService) updateUserOTP(phoneNumber string, newOTP int) error {
+    // Prepare the SQL statement to update the OTP for the user with the given phone number
+    sqlStatement := `
+        UPDATE users
+        SET otp = $1, otp_created_at = NOW()
+        WHERE phone_number = $2
+    `
+
+    // Execute the SQL statement
+    _, err := s.db.DB.Exec(sqlStatement, newOTP, phoneNumber)
+    if err != nil {
+        log.Printf("Error updating OTP in the database: %v", err)
+        return err
+    }
+
+    return nil
+}
+
+// storeUserInDatabase creates a new user in the database with the given phone number and OTP,
+// setting the otp_created_at timestamp to the current time.
+func (s *OTPService) storeUserInDatabase(phoneNumber string, otp int) error {
+    // Prepare the SQL statement to insert a new user with phone number and OTP,
+    // setting the otp_created_at column to the current time.
+    sqlStatement := `
+        INSERT INTO users (phone_number, otp, otp_created_at)
+        VALUES ($1, $2, CURRENT_TIMESTAMP)
+    `
+
+    // Execute the SQL statement
+    _, err := s.db.DB.Exec(sqlStatement, phoneNumber, otp)
+    if err != nil {
+        log.Printf("Error creating a new user and storing OTP in the database: %v", err)
+        return err
+    }
+
+    return nil
 }
