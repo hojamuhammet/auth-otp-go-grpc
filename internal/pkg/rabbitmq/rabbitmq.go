36e30f87fd6c6b70a1af024b753c6731f8f27e9e
kemal <atdayewkemal@gmail.com>
2023-10-23 09:50:39 +0500

    rabbitmq connection function and publishing is added

--- internal/pkg/rabbitmq/rabbitmq.go
+++ internal/pkg/rabbitmq/rabbitmq.go
@@ -7,60 +7,70 @@ import (
 	"github.com/rabbitmq/amqp091-go"
 )
 
-func InitializeRabbitMQ(rabbitMQURL string) (*amqp091.Connection, error) {
-	// Establish a connection to RabbitMQ
-	conn, err := amqp091.Dial(rabbitMQURL)
-	if err != nil {
-		log.Printf("Failed to connect to RabbitMQ: %v", err)
-		return nil, err
-	}
-
-	log.Println("Connected to RabbitMQ")
-
-	return conn, nil
+// RabbitMQService is a service for interacting with RabbitMQ.
+type RabbitMQService struct {
+    conn *amqp091.Connection
 }
 
-func PublishMessage(ctx context.Context, conn *amqp091.Connection, queueName, message string) error {
-	// Create a channel for the connection
-	ch, err := conn.Channel()
-	if err != nil {
-		log.Printf("Failed to open a channel: %v", err)
-		return err
-	}
-	defer ch.Close()
+// NewRabbitMQService initializes a RabbitMQ service.
+func InitRabbitMQConnection(rabbitMQURL string) (*RabbitMQService, error) {
+    // Establish a connection to RabbitMQ
+    conn, err := amqp091.Dial(rabbitMQURL)
+    if err != nil {
+        return nil, err
+    }
 
-	// Declare a queue to ensure it exists
-	_, err = ch.QueueDeclare(
-		queueName, // Queue name
-		true,      // Durable (messages survive server restart)
-		false,     // Delete when unused
-		false,     // Exclusive (only one consumer at a time)
-		false,     // No-wait
-		nil,       // Arguments
-	)
-	if err != nil {
-		log.Printf("Failed to declare a queue: %v", err)
-		return err
-	}
+    log.Println("Connected to RabbitMQ")
+    return &RabbitMQService{conn: conn}, nil
+}
 
-	// Publish the message to the queue
-	err = ch.PublishWithContext(
-		ctx,
-		"",        // Exchange (empty string for default exchange)
-		queueName, // Routing key (queue name)
-		false,     // Mandatory
-		false,     // Immediate
-		amqp091.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(message),
-		},
-	)
-	if err != nil {
-		log.Printf("Failed to publish a message: %v", err)
-		return err
-	}
+// Close closes the RabbitMQ connection.
+func (r *RabbitMQService) Close() {
+    if r.conn != nil {
+        r.conn.Close()
+        log.Println("Closed RabbitMQ connection")
+    }
+}
 
-	log.Printf("Published message to queue %s: %s", queueName, message)
 
-	return nil
+// PublishMessage publishes a message to the specified queue.
+func (r *RabbitMQService) PublishMessage(ctx context.Context, queueName string, message []byte) error {
+    // Create a channel for the connection
+    ch, err := r.conn.Channel()
+    if err != nil {
+        return err
+    }
+    defer ch.Close()
+
+    // Declare a queue to ensure it exists
+    _, err = ch.QueueDeclare(
+        queueName, // Queue name
+        true,      // Durable (messages survive server restart)
+        false,     // Delete when unused
+        false,     // Exclusive (only one consumer at a time)
+        false,     // No-wait
+        nil,       // Arguments
+    )
+    if err != nil {
+        return err
+    }
+
+    // Publish the message to the queue
+    err = ch.PublishWithContext(
+        ctx,
+        "",         // Exchange (empty string for default exchange)
+        queueName,   // Routing key (queue name)
+        false,      // Mandatory
+        false,      // Immediate
+        amqp091.Publishing{
+            ContentType: "application/octet-stream",
+            Body:        message,
+        },
+    )
+    if err != nil {
+        return err
+    }
+
+    log.Printf("Published message to queue %s: %s", queueName, message)
+    return nil
 }
